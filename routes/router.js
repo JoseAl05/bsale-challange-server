31e2163014a48d7d4426d6e305a147775973f8b2
JoseAl05 <jparancibialinker@gmail.com>
2022-11-11 17:23:26 -0300

    add endpoints for API

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const ProductController = require('../controllers/productController');
+const CategoryController = require('../controllers/categoryController');
 
 
 router.get('/products',ProductController.getProducts);
+router.get('/categories',CategoryController.getCategories);
+router.get('/productsByCategory/:id',ProductController.getProductsByCategory);
+router.get('/productsBySearch/:name',ProductController.getProductsBySearch);
 
 
 module.exports = router;
