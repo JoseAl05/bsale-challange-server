d5da113920ec218341a9f91d34cac0e32dcae3d5
JoseAl05 <jparancibialinker@gmail.com>
2022-11-11 17:22:52 -0300

    add getProductByCategory and getProductsBySearch query

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,35 @@
 const Product = require('../models').product;
 const Sequelize = require('sequelize');
+const { Op } = require("sequelize");
 
 module.exports = {
    async getProducts(req,res){
         return await Product
             .findAll()
+            .then(products => res.status(200).json(products))
+            .catch(error => res.status(500).send({error:error}))
+    },
+
+    async getProductsByCategory(req,res){
+        return await Product
+            .findAll({
+                where:{
+                    category:req.params.id
+                }
+            })
+            .then(products => res.status(200).json(products))
+            .catch(error => res.status(500).send({error:error}))
+    },
+
+    async getProductsBySearch(req,res){
+        return await Product
+            .findAll({
+                where:{
+                    name:{
+                        [Op.substring]: req.params.name
+                    }
+                }
+            })
             .then(products => res.status(200).json(products))
             .catch(error => res.status(500).send({error:error}))
     }
